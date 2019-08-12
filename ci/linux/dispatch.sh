c1489ad9956c301ab356b72ac90d1b2a317df194
Tommaso Sartor <tmmsartor@gmail.com>
2019-08-12 17:30:08 +0200

    ci: Fix bash syntax

--- ci/linux/dispatch.sh
+++ ci/linux/dispatch.sh
@@ -29,13 +29,13 @@ if [ "${SECTION}" = 'install' ]; then
 		source "${SCRIPT_DIR}/install_casadi.sh";
 	fi
 
-	if [["${SWIG_PYTHON}" = 'ON' || "${TEMPLATE_PYTHON}" = 'ON']] ;
+	if [[ "${SWIG_PYTHON}" = 'ON' || "${TEMPLATE_PYTHON}" = 'ON']] ;
 	then
 		source "${SCRIPT_DIR}/install_python_dependencies.sh";
 	fi
 
 	if [[ "${SWIG_MATLAB}" = 'ON' ||  "${TEMPLATE_MATLAB}" = 'ON' ]] ||
-	   [["${DEV_MATLAB}" = 'ON' || "${MATLAB_MEX}" = 'ON' ]];
+	   [[ "${DEV_MATLAB}" = 'ON' || "${MATLAB_MEX}" = 'ON' ]];
 	then
 		source "${SHARED_SCRIPT_DIR}/install_matlab.sh";
 	fi
